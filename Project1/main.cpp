91ab9c29982c8ce09ed535ec9a2be9a13abc5732
Sravan <skk7174@g.rit.edu>
2017-06-12 15:31:15 -0400

    Integrated Camera Class.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #include "Shader.h"
+#include "Camera.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -19,10 +20,31 @@ void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 
 // Declaration for Custom Input Processing.
 void processInput(GLFWwindow * window);
+void mouse_callback(GLFWwindow * window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-static float WindowWidth = 800;
-static float WindowHeight = 600;
+#pragma region ScreenRes
+float SCR_WIDTH = 800;
+float SCR_HEIGHT = 600;
+#pragma endregion
 
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+#pragma region GlobalTimeVariables
+
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+#pragma endregion
+
+
+#pragma region MouseGlobals
+
+bool firstMouse = true;
+float lastX = 400, lastY = 300;
+float yaw = -90.0f, pitch = 0.0f, fov = 45.0f;
+
+#pragma endregion
 
 int main() {
 
@@ -33,7 +55,7 @@ int main() {
 
 
 	// Create Window.
-	GLFWwindow * window = glfwCreateWindow(WindowWidth, WindowHeight, "LearnOpenGL", nullptr, nullptr);
+	GLFWwindow * window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
 
 	// Check if the window is created.
 	if (nullptr == window) {
@@ -46,6 +68,8 @@ int main() {
 
 	// Make the current glfw context , the created window.
 	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// Initialise Glad.
 
@@ -60,6 +84,7 @@ int main() {
 
 	// Point the resize function to glfw.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
 
 	// build and compile our shader program
 	// ------------------------------------
@@ -115,6 +140,19 @@ int main() {
 	unsigned int indices[] = {  // note that we start from 0!
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
+	};
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
 
@@ -203,37 +241,32 @@ int main() {
 	glBindVertexArray(0);
 
 
-
-
 	// uncomment this call to draw in wireframe polygons.
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
 	// While the window needn't close,
 	while (!glfwWindowShouldClose(window))
 	{
+
+		// Timer Logic
+		{
+			float currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+		}
+
+
 		// Call the custom Input function every frame.
 		processInput(window);
 
 		{
 
-			// Model / Transformation Matrix.
-			glm::mat4 model;
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-
-			// View Matrix
-			glm::mat4 view;
-			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-			// Projection Matrix
-			glm::mat4 projection;
-			projection = glm::perspective(glm::radians(45.0f), float(WindowWidth / WindowHeight), 0.1f, 100.0f);
-
 			// Rendering Block.
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glEnable(GL_DEPTH_TEST);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 
 
 			// TODO:: binding different textures is a little fuzzy.
@@ -244,10 +277,27 @@ int main() {
 			glActiveTexture(GL_TEXTURE1);
 			glBindTexture(GL_TEXTURE_2D, texture2);
 
+			// View Matrix
+			glm::mat4 view = camera.GetViewMatrix();
+
+			// Projection Matrix
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
 			ourShader.use();
-			ourShader.setMat("model", model);
-			ourShader.setMat("projection", projection);
 			ourShader.setMat("view", view);
+			ourShader.setMat("projection", projection);
+
+			for (unsigned int i = 0; i < 10; i++)
+			{
+				// calculate the model matrix for each object and pass it to shader before drawing
+				glm::mat4 model;
+				model = glm::translate(model, cubePositions[i]);
+				float angle = 20.0f * i;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+				ourShader.setMat("model", model);
+
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
 
 			glBindVertexArray(VAO);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -273,8 +323,8 @@ int main() {
 void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
 	glViewport(0, 0, width, height);
-	WindowWidth = width;
-	WindowHeight = height;
+	SCR_WIDTH = width;
+	SCR_HEIGHT = height;
 }
 
 void processInput(GLFWwindow * window)
@@ -282,6 +332,41 @@ void processInput(GLFWwindow * window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+
+	float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+
+}
+
+void mouse_callback(GLFWwindow * window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
 
 #undef STB_IMAGE_IMPLEMENTATION
