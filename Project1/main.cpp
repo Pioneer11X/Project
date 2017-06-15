e276e34713a2d7ccd5b8bc221748fc383bbb84d1
Sravan <skk7174@g.rit.edu>
2017-06-15 16:06:23 -0400

    Lighting Shaders.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -241,9 +241,17 @@ int main() {
 			ourShader.setMat("view", view);
 			ourShader.setMat("projection", projection);
 			ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-			ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-			ourShader.setVec3("lightPos", lightPos);
+
+			ourShader.setVec3("light.position", lightPos);
+			ourShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+			ourShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+			ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
 			ourShader.setVec3("viewPos", camera.Position);
+			ourShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+			ourShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+			ourShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+			ourShader.setFloat("material.shininess", 32.0f);
 
 			for (unsigned int i = 0; i < 10; i++)
 			{
